eab0640572c8f4dc5411083fda1632697c3a74b2
Adrian Filipescu <adrian.filipescu@gmail.com>
2023-08-31 16:25:56 +0300

    Refactor 2 - currentCategory().

--- src/main/java/trivia/Game.java
+++ src/main/java/trivia/Game.java
@@ -90,17 +90,34 @@ public class Game implements IGame {
    }
 
    public void askQuestion() {
-      if (currentCategory() == "Pop")
+      if (currentCategory().equals("Pop"))
          System.out.println(popQuestions.removeFirst());
-      if (currentCategory() == "Science")
+      if (currentCategory().equals("Science"))
          System.out.println(scienceQuestions.removeFirst());
-      if (currentCategory() == "Sports")
+      if (currentCategory().equals("Sports"))
          System.out.println(sportsQuestions.removeFirst());
-      if (currentCategory() == "Rock")
+      if (currentCategory().equals("Rock"))
          System.out.println(rockQuestions.removeFirst());
    }
 
 
+//   private String currentCategory() {
+//      switch (places[currentPlayer]) {
+//         case 0:
+//         case 4:
+//         case 8:
+//            return "POP";
+//         case 1:
+//         case 5:
+//         case 9:
+//            return "Science";
+//         case 2:
+//         case 6:
+//         case 10:
+//            return "Sports";
+//      }
+//      return "Rock";
+//   }
    private String currentCategory() {
       if (places[currentPlayer] == 0) return "Pop";
       if (places[currentPlayer] == 4) return "Pop";
